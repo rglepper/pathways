04c6d7903a19c8c85bb2ae77dbd256bea68f51e9
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2017-04-11 15:51:25 +0100

    Include a color switch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import {MdToolbarModule, MdCardModule} from '@angular/material';
+import {MdToolbarModule, MdCardModule, MdSlideToggleModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { EscherComponent } from './escher/escher.component';
@@ -21,7 +21,8 @@ import { UploadFileComponent } from './upload-file/upload-file.component';
     FormsModule,
     HttpModule,
     MdToolbarModule,
-    MdCardModule
+    MdCardModule,
+    MdSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/escher/escher.component.ts
+++ src/app/escher/escher.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input,ViewChild, ElementRef, AfterViewInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, AfterViewInit, ViewEncapsulation } from '@angular/core';
 import * as escher from 'escher-vis';
 import * as $ from 'jquery';
 
@@ -7,19 +7,30 @@ import * as $ from 'jquery';
   templateUrl: './escher.component.html',
   encapsulation: ViewEncapsulation.None,
   styleUrls: [
-    '../../../node_modules/escher-vis/css/dist/builder.css' // this why you import css as string
-  ],
+    '../../../node_modules/escher-vis/css/dist/builder.css',
+    './escher.component.css'
+  ]
 })
 export class EscherComponent implements AfterViewInit {
 @ViewChild('pathway') pathwayEl: ElementRef;
-
+builder;
+green = false;
+selection: any;
   @Input() mapData: object;
   @Input() modelData: object = null;
   @Input() embeddedCss: string = null;
-  @Input() options: object = { menu: 'zoom', fill_screen: true };
+  @Input() options: object = {
+    menu: 'zoom',
+    fill_screen: false
+    };
 
   ngAfterViewInit() {
-    escher.Builder(this.mapData, this.modelData, this.embeddedCss, this.pathwayEl.nativeElement, this.options);
+    this.builder = escher.Builder(this.mapData, this.modelData, this.embeddedCss, this.pathwayEl.nativeElement, this.options);
   }
-
+  toggleStyles() {
+    this.green = !this.green;
+    this.builder.selection.selectAll('.segment').style('stroke', (d) => {
+      return this.green ? 'green' : '#334E75';
+    });
+  }
 }
