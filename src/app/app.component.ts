474692e7448e004377da599211ea8a6be481f669
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2017-04-12 09:51:57 +0100

    List node_types and number of nodes for each

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
-
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -7,17 +7,21 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 })
 export class AppComponent {
   file: any;
+  nodes: any = [];
 
   onFileLoaded(event) {
     this.readJSON(event.target.files[0]);
+
   }
 
   readJSON(file) {
         const reader = new FileReader();
         reader.onload = (event) => {
           this.file = JSON.parse(reader.result);
+          this.nodes = _.map(_.groupBy(this.file[1].nodes, 'node_type'), (nodes, type) => {
+            return { 'nodeType': type, 'size': nodes.length };
+          });
         };
         reader.readAsText(file);
-
     }
 }
