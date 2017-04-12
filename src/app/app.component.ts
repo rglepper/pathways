03c4c2b8038025173e3b4f81eb43a8e5c9eda2c1
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2017-04-12 11:10:18 +0100

    List genes that are part of more than one reaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import * as _ from 'lodash';
 export class AppComponent {
   file: any;
   nodes: any = [];
+  genes: any = [];
 
   onFileLoaded(event) {
     this.readJSON(event.target.files[0]);
@@ -22,6 +23,14 @@ export class AppComponent {
                       .groupBy('node_type')
                       .map((nodes, type) => ({'nodeType': type, 'size': nodes.length}))
                       .value();
+          this.genes = _(this.file[1].reactions)
+                      .map(reaction => reaction.genes)
+                      .flattenDeep()
+                      .countBy('name')
+                      .map((count, name) => ( count > 1 ? { name: name, count: count } : false))
+                      .reject(o => !o)
+                      .value();
+
         };
         reader.readAsText(file);
     }
